fd6de72f02a2630eeafccd2d929077ac11875829
andres <carlos.andres.g.g.desarrollo@gmail.com>
2021-08-13 17:05:26 -0500

    php connection

--- Model/Objet/Connection.php
+++ Model/Objet/Connection.php
@@ -1,7 +1,31 @@
 <?php
 
+/*
+*    Autor: Carlos Andres González Gómez  
+*    Description; Class of connection MYsql
+**/
 class Conecction {
 
 
+    // var
+    private $Server;
+    private $User;
+    private $Password;
+    private $Database;
+
+
+
+    protected function __cosntruct(){
+
+    }
     
+
+    protected function Open(){
+
+    }
+
+    
+    protected function closet(){
+
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
+## 
+Autor: Carlos Andres González Gómez 
+
+
 # -AdmissionExamCarlosAndres
 CRUD made under Template Argon and MVC Architecture
+
+
+#
+Installation 
+
+1° Install Docker and Docker-compose
+2° Run Command docker-compose up 
+3° Finish
